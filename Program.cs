45b80495ebe18a8904ade0817bdd1a002705447a
HenryKasai <henrykasai1775@gmail.com>
2020-04-28 22:20:35 -0300

    Exercício resolvido.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira os dados em metros de seu triângulo retângulo.");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Base:"); 
+            Console.ForegroundColor = ConsoleColor.Red;
+            string basey = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Altura:");
+            Console.ForegroundColor = ConsoleColor.Red;
+            string altura = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            decimal base2 = Convert.ToDecimal(basey);
+            decimal altura2 = Convert.ToDecimal(altura);
+            decimal result = base2 * altura2;
+            int dois = 2;
+            decimal resultado = result / dois;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("A área do seu triângulo retângulo é " + resultado + " metros.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            
+
+        }
+    }
+}
