f5eae637c0cfbc7a671852d6b8a5344c8735ddf5
HenryKasai <henrykasai1775@gmail.com>
2020-04-28 22:23:15 -0300

    Exercício resolvido.

--- Program.cs
+++ Program.cs
@@ -19,8 +19,7 @@ namespace AreaTrianguloRet
             decimal base2 = Convert.ToDecimal(basey);
             decimal altura2 = Convert.ToDecimal(altura);
             decimal result = base2 * altura2;
-            int dois = 2;
-            decimal resultado = result / dois;
+            decimal resultado = result / 2;
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("A área do seu triângulo retângulo é " + resultado + " metros.");
             Console.ForegroundColor = ConsoleColor.Gray;
